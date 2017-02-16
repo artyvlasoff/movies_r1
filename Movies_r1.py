befa209c7d91b61a0a25a9c4d39bac3c5484bced
artyvlasoff <artyvlasoff@gmail.com>
2017-02-16 23:14:38 +0300

    Create Movies_r1.py

---
+++ Movies_r1.py
@@ -0,0 +1,82 @@
+import urllib.request
+import urllib.parse
+import json
+import re
+
+
+def load_json_data_from_url(base_url, url_params):
+    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
+    response = urllib.request.urlopen(url).read().decode('utf-8')
+    return json.loads(response)
+
+
+def make_tmdb_api_request(method, api_key, extra_params=None):
+    extra_params = extra_params or {}
+    url = 'https://api.themoviedb.org/3%s' % method
+    params = {
+        'api_key': api_key,
+        'language': 'ru',
+    }
+    params.update(extra_params)
+    return load_json_data_from_url(url, params)
+
+
+# Формирование базы фильмов:
+# --------------------------------------------------------
+
+def create_bd():
+    movies_list = []
+    ed = int(input('Please, enter required value of database: '))
+    num = 0
+
+    while num < ed:
+        try:
+            method_str_group = '/movie/%s' % num
+            rct = make_tmdb_api_request(method=method_str_group, api_key='aa7971b2460ccafcd6d0d480db1b5b97')
+            num += 1
+            movies_list.append(rct)
+            print(num, rct)
+        except urllib.error.HTTPError:
+            num += 1
+            ed += 1
+    print(movies_list)
+
+
+# Сохранение и загрузка в json
+# --------------------------------------------------------
+
+def write_json_bd():
+    with open('movie_data.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(movies_list, ensure_ascii=False))
+
+def read_json_bd():
+    with open('movie_data.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+
+# Поиск фильма по названию
+# --------------------------------------------------------
+
+def find_film():
+    ipt = str(input('Enter the title: '))
+    for i in range(0, len(data)):
+        temp = re.findall(ipt, data[i]['title'], re.IGNORECASE)
+        if len(temp) > 0:
+            print(' -> ', data[i]['title'])
+
+
+# Рекомендации фильмов по названию
+# --------------------------------------------------------
+
+def request_film():
+    global genres_list
+    genres_list = []
+    input_movie = str(input('Enter the title: '))
+    for i in range(0, len(data)):
+        temp = re.findall(ipt, data[i]['title'], re.IGNORECASE)
+        if len(temp) > 0:
+            m_genre = data[i]['genres']
+            for j in range(0, len(data[i]['genres'])):
+                genres_list.append(data[i]['genres'][j]['name'])
+    print(genres_list)
+
